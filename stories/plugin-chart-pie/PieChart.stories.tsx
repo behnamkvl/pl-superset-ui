7041107d6b33971d417b03b0a253d82953fc66d2
Behnam <behnamkvl@gmail.com>
2021-08-05 20:59:56 +0430

    test

--- stories/plugin-chart-pie/PieChart.stories.tsx
+++ stories/plugin-chart-pie/PieChart.stories.tsx
@@ -25,7 +25,7 @@ import { legendTopPercentage } from '../../plugins/plugin-chart-pie/test/__mocks
 import { extractTransformProps } from '../utils';
 
 export default {
-  title: 'Plugins/Pie Chart',
+  title: 'Plugins/Pie Chart test',
   component: PieChart,
   parameters: {
     chromatic: { delay: 2000 },
