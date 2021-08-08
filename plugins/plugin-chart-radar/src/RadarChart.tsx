edeb0346c934224bdec12a4b8d849070e36751d9
behnamkvl <behnamkvl@gmail.com>
2021-08-08 13:04:18 +0430

    color

--- plugins/plugin-chart-radar/src/RadarChart.tsx
+++ plugins/plugin-chart-radar/src/RadarChart.tsx
@@ -208,8 +208,8 @@ const RadarChart: FC<RadarProps<string, string>> = memo(props => {
           <RechartsRadar {...radarProps} stroke="#82ca9d" fill="#82ca9d" fillOpacity={0.6}>
           </RechartsRadar>
         )} */}
-      <RechartsRadar dataKey={radarProps.dataKey} stroke="#8224d8" fill="#2284d8" fillOpacity={0.2} />
-      <RechartsRadar dataKey={radarProps.dataKey2} stroke="#8884d8" fill="#8884d8" fillOpacity={0.2} />
+      <RechartsRadar dataKey={radarProps.dataKey} stroke="#1df5a9" fill="#6ddeb6" fillOpacity={0.1} />
+      <RechartsRadar dataKey={radarProps.dataKey2} stroke="#f77e8c" fill="#f7a1ab" fillOpacity={0.1} />
       <Legend />
       </RechartsRadarChart>
     </Styles>
